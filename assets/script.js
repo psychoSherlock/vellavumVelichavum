6a4c06f78f0ea455f5095ff331c270abf1b8b02e
Athul Prakash NJ <pschosherlock@gmail.com>
2021-07-23 08:39:52 +0530

    Added shaking effect to nav contact icon

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,10 @@
-// Nav bar
+//shake contacts icon every x seconds
 
+shakeTimeOut = 8000
+
+const contactIcon = document.getElementById('contact__icon')
+
+
+setInterval(function () {
+    contactIcon.classList.toggle('shake')
+},shakeTimeOut)
