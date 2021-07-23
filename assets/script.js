491d212f782155c2ddb9d7da8b9433aea6f4d412
Athul Prakash NJ <pschosherlock@gmail.com>
2021-07-23 17:23:51 +0530

    Responsive: navbar and cards

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 //shake contacts icon every x seconds
 
-shakeTimeOut = 8000
+shakeTimeOut = 5000
 
 const contactIcon = document.getElementById('contact__icon')
 
@@ -10,4 +10,36 @@ setInterval(function () {
 },shakeTimeOut)
 
 
+//-----------------Nav on scroll-------------------------------
 
+$(document).ready(function() {// onready
+  
+    var scrollLink = $('.nav__link');
+    
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top //if cliked,scroll with slide effect
+      }, 1000 );
+    });
+    
+    // Active link switching
+    $(window).scroll(function() { //on scroll
+      var scrollbarLocation = $(this).scrollTop(); //scrollBar location
+      
+      scrollLink.each(function() {// for each items in scroll link
+        
+        var sectionOffset = $(this.hash).offset().top - 20; //get offset of that hash
+        
+        if ( sectionOffset <= scrollbarLocation ) {
+          $(this).addClass('nav_active'); //switch classes
+          $(this).siblings().removeClass('nav_active');
+        }
+      })
+      
+    })
+    
+  })
+
+//-----------------Nav on scroll-------------------------------
